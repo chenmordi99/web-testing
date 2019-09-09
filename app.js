60aa51588dc36b80d24b02bac390f8d377a85129
Chen Mordechai <chenmordi99@gmail.com>
2019-09-09 19:29:40 +0300

    /account/create API improved

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-const express = require('express');
+const express = require('express'); 
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Account = require('./models/account/Account');
 
 mongoose.connect('mongodb://localhost:27017/codeTest', {
 	autoReconnect: true,
 	reconnectTries: 60,
-	reconnectInterval: 10000
+	reconnectInterval: 10000,
+	useNewUrlParser: true
 });
 
 const app = express();
 app.listen(3000);
 
-app.use(require('body-parser').json());
-app.use('/account/create', require('./api/account/create'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/account/create', require('./api/account/create')); //task1
+
+
 
 console.log('app running on port 3000...');
-
 module.exports = app;

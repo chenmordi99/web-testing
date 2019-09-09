b733e5fd8a54f43d0ecef3303d1c346ee5fe376b
Chen Mordechai <chenmordi99@gmail.com>
2019-09-09 19:40:00 +0300

    /account/create was improved                                                                                                                                                                                                                                               it returns an error if account with same email

--- api/account/create.js
+++ api/account/create.js
@@ -3,6 +3,16 @@ const Account = require('../../models/account/Account');
 module.exports = async function(req, res, next) {
 	const {email, name, age} = req.body;
 	const account = new Account({email, name, age});
-	await account.save();
-	return res.send({message: 'success'});
+	//1.2
+	try{
+		const searchRes = await Account.findOne({email: req.body.email});
+		if(searchRes != null){
+			return res.send({error: 'email already exists'});
+		} else {
+			await account.save();
+			return res.send({message: 'success'});
+		}
+	} catch(e) {
+		console.log(e);
+	}
 };
